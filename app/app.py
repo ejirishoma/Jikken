1ea5ca84419e3137cee8ce5acc3a28d454c3e5c8
ukyo0715 <ukyoo0715@gmail.com>
2022-06-20 14:47:25 +0900

    Ukyo

--- app/app.py
+++ app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config.from_object('config')
-
-db = SQLAlchemy(app) 
-from models import user
-
-import views
-
 if __name__ == '__main__':
     app.run(host='localhost')
+
+@app.route('/')
+def index():
+        return 'Hellow World!'
